a19367d49e16443df05552550a80c2a49372e5c6
Tareq-dev <tareque.dev@gmail.com>
2023-07-27 14:38:55 +0600

    get products

--- controllers/api_s.js
+++ controllers/api_s.js
@@ -1,2 +1,14 @@
 const { connect } = require("../config/db");
 const { ObjectId } = require("mongodb");
+
+//     // GET == products
+
+module.exports.getProducts = async (req, res, next) => {
+    try {
+      const db = await connect();
+      const result = await db.collection("products").find().toArray();
+      res.json({ success: true, result });
+    } catch (error) {
+      next(error);
+    }
+  };
